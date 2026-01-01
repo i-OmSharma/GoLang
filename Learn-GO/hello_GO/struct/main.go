556baf7031126eb6584ce505254d0ff6bc428a49
i-OmSharma <sharmaom1201@gmail.com>
2026-01-01 23:10:10 +0530

    struct new operator

--- Learn-GO/hello_GO/struct/main.go
+++ Learn-GO/hello_GO/struct/main.go
@@ -16,15 +16,22 @@ func main() {
 	Om.Age = 21
 
 	fmt.Println("Person Om", Om)
-	
+
 	// Second Method
 	Person1 := Person {
 		FristName: "Akash",
 		LastName: "Bikash",
 		Age: 23,
 	}
-	
+
 	fmt.Println("Person1 details : ", Person1)
-	
-	
+
+	// Third Method (new Keyword)
+	var person2 = new(Person)
+	person2.FristName = "Agarwal"
+	person2.LastName = "Singh"
+	person2.Age = 44
+
+	fmt.Println("Person2 with new keyword: ", person2)
+
 }
