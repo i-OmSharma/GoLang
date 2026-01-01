352256202a86a5c7c4261886d1414d711dc3de1f
i-OmSharma <sharmaom1201@gmail.com>
2026-01-01 22:55:06 +0530

    struct

---
+++ Learn-GO/hello_GO/struct/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	FristName string
+	LastName  string
+	Age       int
+}
+
+func main() {
+	var Om Person
+
+	Om.FristName = "Om"
+	Om.LastName = "SHA"
+	Om.Age = 21
+
+	fmt.Println("Person Om", Om)
+	
+	
+}
